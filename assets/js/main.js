c1821bbd15e9e5f08c6d89c5de9fa1e11b3390ae
Ketlin de lima <ketlin@senocwb.com>
2023-12-08 08:18:08 -0300

    Local storage function

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,14 +14,14 @@ function createTask(textInput) {
     tasks.appendChild(li);
     cleanInput();
     createDelButton(li);
-    saveTask();
+    saveTask("oi");
 }
 
 // Enable button function
 btnTask.addEventListener('click', function() {
     if (!inputTask.value) return;
     createTask(inputTask.value);
-})
+});
 
 // Enable Enter command
 inputTask.addEventListener('keypress', function(e){
@@ -29,13 +29,13 @@ inputTask.addEventListener('keypress', function(e){
         if (!inputTask.value) return;
         createTask(inputTask.value);
     }
-})
+});
 
 // Clean input 
 function cleanInput() {
     inputTask.value = '';
     inputTask.focus();
-}
+};
 
 function createDelButton(li) {
     li.innerText += " ";
@@ -43,7 +43,7 @@ function createDelButton(li) {
     delButton.innerText = 'x';
     delButton.setAttribute('class', 'del')
     li.appendChild(delButton);
-}
+};
 
 // Listener del button click
 document.addEventListener('click', function(e){
@@ -51,10 +51,12 @@ document.addEventListener('click', function(e){
 
     if (el.classList.contains('del')){
         el.parentElement.remove();
+        saveTask();
     }
-})
+});
 
-function saveTask() {
+function saveTask(oi) {
+    console.log(oi);
     const liTasks = tasks.querySelectorAll('li');
     const taskList = [];
 
@@ -64,5 +66,18 @@ function saveTask() {
         taskList.push(textTask);
     }
     const tasksJSON = JSON.stringify(taskList);
+    localStorage.setItem('tasks', tasksJSON);
     console.log(tasksJSON);
 }
+
+// Get 
+function addLocalTasks() {
+    const tasks = localStorage.getItem('tasks');
+    const taskList = JSON.parse(tasks);
+
+    for(let task of taskList){
+        createTask(task);
+    }
+}
+
+addLocalTasks();
